91eb1f9b8abaa05076dec480294f9be0161d0774
yourboyangel <eabazaj22@epoka.edu.al>
2023-12-10 17:42:43 +0100

    Finishing queries by Engjell and Kristi

--- queriess/Queries.sql
+++ queriess/Queries.sql
@@ -60,3 +60,72 @@ SELECT ProgramYears.StartYear, COUNT(ProgramEnrollment.StudentID) AS StudentCoun
 FROM ProgramYears
 LEFT JOIN ProgramEnrollment ON ProgramYears.ProgramYearID = ProgramEnrollment.ProgramYearID
 GROUP BY ProgramYears.StartYear;
+
+--Find the department with the biggest number of students
+SELECT d.DepartmentName, COUNT(pe.StudentID) AS EnrolledStudents
+FROM Departments d
+JOIN Programs p ON d.DepartmentID = p.DepartmentID
+JOIN ProgramYears py ON p.ProgramID = py.ProgramID
+JOIN ProgramEnrollment pe ON py.ProgramYearID = pe.ProgramYearID
+GROUP BY d.DepartmentName
+ORDER BY EnrolledStudents DESC
+LIMIT 1;
+
+--Top 5 lecturers with the biggest number of students in their classes
+SELECT l.LecturerName, COUNT(cs.StudentID) AS EnrolledStudents
+FROM Lecturers l
+JOIN LecturerClass lc ON l.LecturerID = lc.LecturerID
+JOIN Classes c ON lc.ClassID = c.ClassID
+JOIN CourseSelection cs ON c.ClassID = cs.ClassID
+GROUP BY l.LecturerName
+ORDER BY EnrolledStudents DESC
+LIMIT 5;
+
+--Lecturer who has taught most classes
+SELECT l.LecturerName, COUNT(lc.ClassID) AS TaughtClassesCount
+FROM Lecturers l
+JOIN LecturerClass lc ON l.LecturerID = lc.LecturerID
+GROUP BY l.LecturerName
+ORDER BY TaughtClassesCount DESC
+LIMIT 1;
+
+--Student with the highest scholarship
+SELECT StudentName, Scolarship
+FROM Students
+ORDER BY Scolarship DESC
+LIMIT 1;
+
+--Total credits earned by each student
+SELECT s.StudentID, s.StudentName, SUM(c.Credits) AS TotalCreditsEarned
+FROM Students s
+JOIN CourseSelection cs ON s.StudentID = cs.StudentID
+JOIN Classes cl ON cs.ClassID = cl.ClassID
+JOIN Courses c ON cl.CourseID = c.CourseID
+GROUP BY s.StudentID, s.StudentName;
+
+--Students who have paid more than average payment
+SELECT s.StudentName, p.PaymentAmount, py.ProgramYearID
+FROM Students s
+JOIN Payments p ON s.StudentID = p.StudentID
+JOIN ProgramEnrollment pe ON s.StudentID = pe.StudentID
+JOIN ProgramYears py ON pe.ProgramYearID = py.ProgramYearID
+WHERE p.PaymentAmount > (
+    SELECT AVG(p2.PaymentAmount)
+    FROM Payments p2
+    WHERE p2.ProgramYearID = py.ProgramYearID
+);
+
+--Email addresses of professors of a department
+SELECT DISTINCT le.Email
+FROM Lecturers le
+INNER JOIN Departments d ON le.DepartmentID = d.DepartmentID
+WHERE d.DepartmentName = 'Computer Engineering';
+
+--Results of top performing students
+SELECT fg.CourseID, c.Name AS CourseName, s.StudentName, MAX(fg.Val) AS Final_Grade
+FROM FinalGrade fg
+JOIN Students s ON fg.StudentID = s.StudentID
+JOIN Courses c ON fg.CourseID = c.CourseID
+GROUP BY fg.CourseID, c.Name, s.StudentName;
+
+
